9def52b5d2253e7595be984e3be20e2e252baa1c
Append <appendtrue@gmail.com>
2020-03-03 15:40:42 +0800

    Add web-tinker package

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -25,6 +26,14 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('viewWebTinker', function (User $user = null) {
+            if ($user && in_array($user->email, [
+                'fansheng0594@163.com',
+            ])) {
+                return true;
+            } else {
+                return false;
+            }
+        });
     }
 }
